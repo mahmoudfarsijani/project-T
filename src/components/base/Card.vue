88c0bd3b0d320f0b20ce0c16e62fd5a0b25b0b7c
mahmud farsi <mhfarsi002022@gmail.com>
2024-08-29 12:56:44 +0330

    add card component and edit in tailwind-config

---
+++ src/components/base/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <component
+    :is="tag"
+    :class="[
+      'cart',
+      `cart--${isRow ? 'row' : 'col'}`,
+      `cart--${isRounded ? 'rounded' : 'notrounded'}`,
+      `cart--${isRouter ? 'router' : 'static'}`,
+      elementClass
+    ]"
+  >
+    <div v-if="$slots.header" class="header">
+      <slot name="header" />
+    </div>
+    <slot />
+    <div v-if="$slots.footer" class="footer">
+      <slot name="footer" />
+    </div>
+  </component>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+import { oneOf, bool } from 'vue-types'
+
+const props = defineProps({
+  tag: oneOf(['li', 'div', 'section', 'span', 'RouterLink']).def('div'),
+  isRow: bool().def(false),
+  isRounded: bool().def(false),
+  isRouter: bool().def(false),
+  isWhite: bool().def(false),
+  isDark: bool().def(false)
+})
+
+const elementClass = computed(() => ({
+  'cart--white': props.isWhite,
+  'cart--dark': props.isDark,
+}))
+</script>
+
+<style scoped>
+.cart {
+  @apply flex;
+}
+
+.cart--col {
+  @apply flex-col;
+}
+
+.cart--row {
+  @apply flex-row;
+}
+
+.cart--rounded {
+  @apply rounded-md;
+}
+
+.cart--norounded {
+  @apply rounded-none;
+}
+
+.cart--router {
+  @apply cursor-pointer;
+}
+
+.cart--static {
+  @apply cursor-default;
+}
+
+.cart--white {
+    @apply bg-gray-100
+}
+
+.cart--dark {
+    @apply bg-slate-900
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      container:{
+      container: {
         center: true
       }
-    },
+    }
   },
-  plugins: [
-    require('daisyui')
-  ],
+  plugins: [require('daisyui')],
   daisyui: {
     theme: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-      "dim",
-      "nord",
-      "sunset",
+      'winter','night'
     ]
   }
 }
-
