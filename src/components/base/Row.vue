c51ef5e3e04339048d0bece37b676267f24c3a23
mahmud farsi <mhfarsi002022@gmail.com>
2024-08-29 14:05:20 +0330

    add row component

---
+++ src/components/base/Row.vue
@@ -0,0 +1,41 @@
+<template>
+  <component
+    :is="tag"
+    :class="['list', `list--${isCol ? 'col' : 'row'}`, `list--${isNoWrap ? 'nowrap' : 'wrap'}`]"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import {  oneOf, bool } from 'vue-types'
+
+const props = defineProps({
+  tag: oneOf(['div', 'section', 'ul']).def('div'),
+  isCol: bool().def(false),
+  isNoWrap: bool().def(false)
+})
+</script>
+
+<style scoped>
+.list {
+  @apply flex;
+}
+
+.list--col {
+  @apply flex-col;
+}
+
+.list--row {
+  @apply flex-row;
+}
+
+.list-nowrap {
+  @apply flex-nowrap;
+}
+
+.list--wrap {
+  @apply flex-wrap;
+}
+</style>
