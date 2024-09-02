a328ca20f86b481e9d95efec31d6e08bf7f7c253
mahmud farsi <mhfarsi002022@gmail.com>
2024-09-02 22:45:59 +0330

    add contaienr component to shopStore

--- src/views/store/child/ShopStore.vue
+++ src/views/store/child/ShopStore.vue
@@ -1,9 +1,15 @@
 <template>
-    <Main class="pt-[200px]">
-        shop store
-    </Main>
+  <Main class="pt-[200px]">
+    <Container>
+        {{ dataStore }}
+    </Container>
+  </Main>
 </template>
 
 <script setup>
- import Main from '@/components/base/Main.vue';
+import Main from '@/components/base/Main.vue'
+import { useStoreFetch } from '@/composables/useStoreFetch.js'
+import Container from '@/components/base/Container.vue'
+
+const { dataStore, errorStore, isLoading } = useStoreFetch()
 </script>
