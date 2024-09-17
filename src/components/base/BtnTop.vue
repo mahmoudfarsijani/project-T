066fda5d23828da8138db47c572fca26785743d6
mahmud farsi <mhfarsi002022@gmail.com>
2024-09-17 16:50:08 +0330

    add btn component to button top and add style

--- src/components/base/BtnTop.vue
+++ src/components/base/BtnTop.vue
@@ -1,16 +1,20 @@
 <template>
-  <button
-    class="fixed right-[20px] bottom-[20px] bg-red-600 p-[20px]"
+  <Button
+    class="fixed right-[20px] bottom-[20px]  p-[20px]"
     v-if="showBtn"
+    :is-icon-only="true"
+    icon="arrow-up"
+    :is-rounded-full="true"
+    :is-primary="true"
     @click="scrollHandler"
-  >
-    up
-  </button>
+  />
+
 </template>
 
 <script setup>
 import { onUnmounted, onMounted, ref } from 'vue'
 import { useEventListener } from '@vueuse/core'
+import Button from '@/components/base/Button.vue';
 
 const showBtn = ref(false)
 const scrollEvent = () => {
