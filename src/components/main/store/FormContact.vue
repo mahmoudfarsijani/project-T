6836e002a0dcbd92c99614353401d57a84a139ff
mahmud farsi <mhfarsi002022@gmail.com>
2024-09-18 14:03:07 +0330

    add reactive v-model to inputs in formcontact

--- src/components/main/store/FormContact.vue
+++ src/components/main/store/FormContact.vue
@@ -1,26 +1,51 @@
 <template>
     <form @submit.prevent="formHandler" class="box">
         <div class="box__input">
-            <input type="text" class="box__input--style" placeholder="Your Name">
+            <input type="text" v-model="infos.username" class="box__input--style" placeholder="Your Name">
         </div>
         <div class="box__input">
-            <input type="text" class="box__input--style" placeholder="Your Email">
+            <input type="email" v-model="infos.email" class="box__input--style" placeholder="Your Email">
         </div>
         <div class="box__input">
-            <input type="text" class="box__input--style" placeholder="Subject">
+            <input type="text" v-model="infos.subject" class="box__input--style" placeholder="Subject">
         </div>
         <div class="box__input">
             <textarea placeholder="Message" class="w-full bg-gray-300 p-[10px] resize-none h-[200px]"></textarea>
         </div>
         <Button tag="button" type="submit" :is-icon-only="false" :is-yellow="true">
-            send message
+            Send Message
         </Button>
     </form>
 </template>
 
 
 <script setup>
+    import { ref,reactive } from 'vue';
     import Button from '@/components/base/Button.vue'
+ 
+
+    
+    // const username = ref('')
+    // const email = ref('')
+    // const subject = ref('')
+
+
+    const infos = reactive({
+        username: '',
+        email: '',
+        subject: ''
+    });
+
+    const formHandler = () => {
+        console.log(JSON.stringify(infos));
+        infos.username = ''
+        infos.email = ''
+        infos.subject = ''
+    }
+
+
+
+
 </script>
 
 <style scoped>
