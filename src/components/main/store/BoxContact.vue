ee3f74411ec7e633b63b0bdb654cb8e55e499b9c
mahmud farsi <mhfarsi002022@gmail.com>
2024-09-18 13:30:28 +0330

    add contact and add iframe for map

--- src/components/main/store/BoxContact.vue
+++ src/components/main/store/BoxContact.vue
@@ -1,12 +1,11 @@
 <template>
-    <div>
-
-        <FormContact/>
-        
-    </div>
+  <div class="w-full flex flex-nowrap gap-[20px]">
+    <FormContact class="flex-1"/>
+    <InfoContact class="flex-1"/>
+  </div>
 </template>
 
-
 <script setup>
-    import FormContact from '@/components/main/store/FormContact.vue'
+import FormContact from '@/components/main/store/FormContact.vue'
+import InfoContact from '@/components/main/store/InfoContact.vue'
 </script>
--- src/components/main/store/InfoContact.vue
+++ src/components/main/store/InfoContact.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        <div></div>
-        <div></div>
+    <div class="flex flex-col justify-start gap-[20px] bg-gray-100">
+        <div class="p-[20px]">
+            <iframe width="100%" height="250px" loading="lazy" :src="googleMap"></iframe>
+        </div>
+        <div class="flex flex-col gap-[15px] p-[20px]">
+            <div class="flex flex-nowrap gap-[10px]">
+                <Icon name="loc"/>
+                <span class="text-[14px]">123 Street, New York, USA</span>
+            </div>
+            <div class="flex flex-nowrap gap-[10px]">
+                <Icon name="gmail"/>
+                <span class="text-[14px]">info@example.com</span>
+            </div>
+            <div class="flex flex-nowrap gap-[10px]">
+                <Icon name="phone"/>
+                <span class="text-[14px]">+012 345 67890</span>
+            </div>
+        </div>
     </div>
 </template>
+
+
+<script setup>
+    import Icon from '@/components/base/Icon.vue'
+
+    const googleMap = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509367!2d144.95373531531956!3d-37.81720997975171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577ed9165b2c151!2sFederation%20Square%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2sus!4v1614291166742!5m2!1sen!2sus'
+</script>
