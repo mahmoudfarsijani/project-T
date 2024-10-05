73b21805e104b0ea0caaf250755e78d7086ac203
mahmud farsi <mhfarsi002022@gmail.com>
2024-10-05 15:45:11 +0330

    add file seconedetailmovie and add props in seconedetailmovie

---
+++ src/components/main/movie/detailsSection/SecOneDetailMovie.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref, computed } from 'vue'
+import { string, object, array, shape, number,bool } from 'vue-types'
+import Img from '@/components/base/Img.vue'
+import Row from '@/components/base/Row.vue'
+
+
+const props = defineProps({
+    detail:shape({
+        adult:bool(),
+        backdrop_path:string(),
+        budget:string(),
+        genres:array(),
+        homepage:string(),
+        id:number(),
+        imdb_id:number(),
+        origin_country:array(),
+        original_language:string(),
+        original_title:string(),
+        overview:string(),
+        popularity:number(),
+        poster_path:string(),
+        production_companies:array(),
+        production_countries:array(),
+        release_date:string(),
+        spoken_languages:array(),
+        status:string(),
+        tagline:string(),
+        title:string(),
+        vote_average:number(),
+        vote_count:number()
+    })
+})
+</script>
--- src/views/movie/child/DetailMovie.vue
+++ src/views/movie/child/DetailMovie.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="text-white bg-red-500">
-    detail movie
-    {{ dataDetailMovie}}
-  </div>
+  <Main>
+    <Container class="text-white bg-red-500">
+      detail movie
+      {{ dataDetailMovie }}
+    </Container>
+  </Main>
 </template>
 
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
 import { useFetchDetailMovie } from '@/composables/useDetailMovie.js'
-
+import Container from '@/components/base/Container.vue'
+import Main from '@/components/base/Main.vue'
 
 const router = useRouter()
 const route = useRoute()
-const {dataDetailMovie,isLoadingDetailMovie,errorDetailMovie} = useFetchDetailMovie(route.params.id)
+const { dataDetailMovie, isLoadingDetailMovie, errorDetailMovie } = useFetchDetailMovie(
+  route.params.id
+)
 </script>
