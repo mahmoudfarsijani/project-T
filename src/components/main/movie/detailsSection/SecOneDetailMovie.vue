661fbd9f0b7f553ae4eaf356f5942671ee6d0e2c
mahmud farsi <mhfarsi002022@gmail.com>
2024-10-05 19:04:48 +0330

    add icon for imadb and add title and add overview and add runtime to movie

--- src/components/main/movie/detailsSection/SecOneDetailMovie.vue
+++ src/components/main/movie/detailsSection/SecOneDetailMovie.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
+    <div v-if="detail" class="flex smm:flex-col md:flex-row lg:flex-row w-full gap-[50px]" >
+        <!-- <div class="smm:w-[60%] md:w-[35%] max-w-[250px] smm:mx-auto md:mx-0">
+            <div class="w-full overflow-hidden rounded-md">
+                <Img :src="srcImg" :alt="detail.title" class="w-full h-full"/>
+            </div>
+        </div> -->
+        <div>
+            <h1 class="title smm:text-[37px] md:text-[37px] lg:text-[49px] font-kanit">
+                {{ detail.title }}
+            </h1>
+            <div class="flex items-center gap-[15px]">
+                <div class="flex flex-nowrap">
+                    <Icon v-for="item in rate" :key="item.id" :name=" item.count <= countRange ? 'fill-star' : 'empty-star' "/>
+                </div>
+                <span class="text-[14px]">
+                    {{ countRange }} (IMDB)  {{ runTime }}
+                </span>
+                <div>
 
+                </div>
+            </div>
+            <p class="subtitle smm:text-[13px] md:text-[15px] lg:text-[17px]">
+                {{ detail.overview }} 
+            </p>
+        </div>
     </div>
 </template>
 
@@ -9,6 +32,8 @@ import { defineProps, ref, computed } from 'vue'
 import { string, object, array, shape, number,bool } from 'vue-types'
 import Img from '@/components/base/Img.vue'
 import Row from '@/components/base/Row.vue'
+import Icon from '@/components/base/Icon.vue'
+import { nanoid } from 'nanoid'
 
 
 const props = defineProps({
@@ -34,7 +59,49 @@ const props = defineProps({
         tagline:string(),
         title:string(),
         vote_average:number(),
-        vote_count:number()
+        vote_count:number(),
+        runtime:number()
     })
 })
+
+// const srcImg = computed(() => {
+//     return `https://image.tmdb.org/t/p/w500/${props.detail.poster_path}`
+// })
+
+const rate = [
+    {
+        count:2,
+        id:nanoid(3)
+    },
+    {
+        count:4,
+        id:nanoid(3)
+    },
+    {
+        count:6,
+        id:nanoid(3)
+    },
+    {
+        count:8,
+        id:nanoid(3)
+    },
+    {
+        count:10,
+        id:nanoid(3)
+    },
+]
+
+const countRange = computed(() => {
+    if(props.detail.vote_count){
+        return Math.floor((props.detail.vote_average)).toFixed(1)
+    }
+})
+
+const runTime = computed(() => {
+    const hours = Math.floor(props.detail?.runtime / 60)
+    const remaining = props.detail?.runtime % 60
+    return `${hours}h: ${remaining}m`
+})
+
+
 </script>
--- src/views/movie/child/DetailMovie.vue
+++ src/views/movie/child/DetailMovie.vue
@@ -1,9 +1,20 @@
 <template>
   <Main>
-    <Container class="text-white bg-red-500">
-      detail movie
-      {{ dataDetailMovie }}
-    </Container>
+    <div
+      class=" min-h-screen bg-no-repeat bg-center"
+      :style="{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4)) ,url('${srcImg}')`,
+        backgroundRepeat: 'norepeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%'
+      }"
+    >
+      <Container class="text-white ">
+        detail movie
+        <SecOneDetailMovie :detail="dataDetailMovie" />
+      </Container>
+    </div>
   </Main>
 </template>
 
@@ -11,11 +22,28 @@
 import { useRouter, useRoute } from 'vue-router'
 import { useFetchDetailMovie } from '@/composables/useDetailMovie.js'
 import Container from '@/components/base/Container.vue'
+import SecOneDetailMovie from '@/components/main/movie/detailsSection/SecOneDetailMovie.vue'
 import Main from '@/components/base/Main.vue'
+import { computed } from 'vue'
 
 const router = useRouter()
 const route = useRoute()
 const { dataDetailMovie, isLoadingDetailMovie, errorDetailMovie } = useFetchDetailMovie(
   route.params.id
 )
+
+const srcImg = computed(() => {
+  if (dataDetailMovie.value.poster_path) {
+    return `https://image.tmdb.org/t/p/w500/${dataDetailMovie.value?.poster_path}`
+  }
+})
+console.log(srcImg)
 </script>
+
+<style>
+.b {
+  background-position: center;
+
+  background-size: cover;
+}
+</style>
