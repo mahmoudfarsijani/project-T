b50d1305f2d0ee892d602c056de8a94b213f3747
mahmud farsi <mhfarsi002022@gmail.com>
2024-10-11 15:29:13 +0330

    add js-cookie in login

--- src/components/main/auth/BoxLogin.vue
+++ src/components/main/auth/BoxLogin.vue
@@ -1,34 +1,65 @@
 <template>
-    <Form :validation-schema="schemaForm" class="w-[100%] max-w-[450px] gap-[50px] flex flex-col items-center bg-gray-700 bg-opacity-50 px-[20px] py-[20px]">
-        <h2 class="title text-gray-50 text-[29px] capitalize">
-            login
-        </h2>
-        <div class="input__box flex flex-col gap-[10px] h-[60px] w-full">
-            <label for="email" class="text-[16px] capitalize text-gray-200"> email </label>
-            <Field as="input" type="email" name="email" id="email" v-model="email" class="w-full px-[8px] py-[5px] text-[18px] bg-gray-200 rounded-md"/>
-            <ErrorMessage name="email" class="text-[11px] text-red-500 capitalize"/>
-        </div>
-        <div class="input__box h-[60px] w-full flex flex-col gap-[10px]">
-            <label for="password" class="text-[16px] capitalize text-gray-200"> password </label>
-            <Field as="input" type="password" name="password" id="password" v-model="password" class="w-full px-[8px] py-[5px] text-[18px] bg-gray-200 rounded-md"/>
-            <ErrorMessage name="password" class="text-[11px] text-red-500 capitalize"/>
-        </div>
-        <button type="submit" class="px-[25px] py-[10px] capitalize bg-teal-700 text-white rounded-lg w-full mt-[20px]">
-            submit
-        </button>
-    </Form>
+  <Form
+    :validation-schema="schemaForm"
+    @submit="formHandler"
+    class="w-[100%] max-w-[450px] gap-[50px] flex flex-col items-center bg-gray-700 bg-opacity-50 px-[20px] py-[20px]"
+  >
+    <h2 class="title text-gray-50 text-[29px] capitalize">login</h2>
+    <div class="input__box flex flex-col gap-[10px] h-[60px] w-full">
+      <label for="email" class="text-[16px] capitalize text-gray-200"> email </label>
+      <Field
+        as="input"
+        type="email"
+        name="email"
+        id="email"
+        v-model="email"
+        class="w-full px-[8px] py-[5px] text-[18px] bg-gray-200 rounded-md"
+      />
+      <ErrorMessage name="email" class="text-[11px] text-red-500 capitalize" />
+    </div>
+    <div class="input__box h-[60px] w-full flex flex-col gap-[10px]">
+      <label for="password" class="text-[16px] capitalize text-gray-200"> password </label>
+      <Field
+        as="input"
+        type="password"
+        name="password"
+        id="password"
+        v-model="password"
+        class="w-full px-[8px] py-[5px] text-[18px] bg-gray-200 rounded-md"
+      />
+      <ErrorMessage name="password" class="text-[11px] text-red-500 capitalize" />
+    </div>
+    <div class="w-full flex flex-nowrap gap-[5px] justify-start">
+      <input type="checkbox" id="remember" v-model="isRemember" />
+      <label for="remember" class="select-none text-white"> Remember Me </label>
+    </div>
+    <button
+      type="submit"
+      class="px-[25px] py-[10px] capitalize bg-teal-700 text-white rounded-lg w-full mt-[20px]"
+    >
+      submit
+    </button>
+    {{ isRemember }}
+  </Form>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref,defineEmits } from 'vue'
 import { Field, Form, ErrorMessage } from 'vee-validate'
 import * as Yup from 'yup'
 
-
 const email = ref('')
 const password = ref('')
+const isRemember = ref(false)
+const emits = defineEmits(['update:select-value'])
 const schemaForm = Yup.object({
-    email: Yup.string().email().required(),
-    password: Yup.string().min(8).required()
+  email: Yup.string().email().required(),
+  password: Yup.string().min(8).required()
 })
+
+const formHandler = () => {
+    if(password.value && email.value){
+        emits('update:select-value',isRemember.value,email.value)
+    }
+}
 </script>
--- src/views/auth/child/Login.vue
+++ src/views/auth/child/Login.vue
@@ -1,13 +1,24 @@
 <template>
   <Main class="bg-gradient-to-tr from-cyan-700 to-slate-800">
     <Container class="h-screen flex justify-center items-center">
-      <BoxLogin />
+      <BoxLogin @update:select-value="selected" />
     </Container>
+    <div class="bg-red-500">
+      {{ val }}
+    </div>
   </Main>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Container from '@/components/base/Container.vue'
 import Main from '@/components/base/Main.vue'
+import  Cookie  from 'js-cookie'
 import BoxLogin from '@/components/main/auth/BoxLogin.vue'
+const val = ref('')
+const selected = (cookie, email) => {
+  if (cookie === true) {
+    Cookie.set('cookie-user', email, { expires: 7 })
+  }
+}
 </script>
